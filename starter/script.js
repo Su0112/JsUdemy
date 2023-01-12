33accee8838b485bacfcb0df76e3d156a86048e9
Su Shruthi <shruthacm@gmail.com>
2023-01-12 00:35:27 -0800

    functions

--- starter/script.js
+++ starter/script.js
@@ -1,7 +1,32 @@
 "use strict";
 
-const yearsUntilRetirement = function (birthYear, firstName) {
-  const age = 2037 - birthYear;
-  const retirement = 65 - age;
-  // return retirement;
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
 };
+const yearsUntilRetirement = function (birthYear, firstName) {
+  const age = calcAge;
+  const retirement = 65 - age;
+
+  if (retirement > 0) {
+    return retirement;
+  } else {
+    return -1;
+  }
+  //return `$(firstName) retires in $(retirement) years`;
+};
+//yearsUntilRetirement(1991, "Jonas");
+console.log(yearsUntilRetirement(1991, "Su"));
+console.log(yearsUntilRetirement(1951, "SuMama"));
+
+//function declaration
+function calcAge(birthYear) {
+  return 2037 - birthYear;
+}
+
+//function expression
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+//Arrow function
+const calcAge = (birthYear) => 2037 - birthYear;
