82b198d4cfc8540b8b35cdd8b0d6f88ad024f969
Su Shruthi <shruthacm@gmail.com>
2023-01-13 23:32:59 -0800

    coding challenge

--- starter/script.js
+++ starter/script.js
@@ -1,32 +1,40 @@
-"use strict";
+// "use strict";
 
-const calcAge = function (birthYear) {
-  return 2037 - birthYear;
-};
-const yearsUntilRetirement = function (birthYear, firstName) {
-  const age = calcAge;
-  const retirement = 65 - age;
+// const calcAge = function (birthYear) {
+//   return 2037 - birthYear;
+// };
+// const yearsUntilRetirement = function (birthYear, firstName) {
+//   const age = calcAge;
+//   const retirement = 65 - age;
 
-  if (retirement > 0) {
-    return retirement;
-  } else {
-    return -1;
-  }
-  //return `$(firstName) retires in $(retirement) years`;
-};
-//yearsUntilRetirement(1991, "Jonas");
-console.log(yearsUntilRetirement(1991, "Su"));
-console.log(yearsUntilRetirement(1951, "SuMama"));
+//   if (retirement > 0) {
+//     return retirement;
+//   } else {
+//     return -1;
+//   }
+//   //return `$(firstName) retires in $(retirement) years`;
+// };
+// //yearsUntilRetirement(1991, "Jonas");
+// console.log(yearsUntilRetirement(1991, "Su"));
+// console.log(yearsUntilRetirement(1951, "SuMama"));
 
-//function declaration
-function calcAge(birthYear) {
-  return 2037 - birthYear;
-}
+// //function declaration
+// function calcAge(birthYear) {
+//   return 2037 - birthYear;
+// }
 
-//function expression
-const calcAge = function (birthYear) {
-  return 2037 - birthYear;
-};
+// //function expression
+// const calcAge = function (birthYear) {
+//   return 2037 - birthYear;
+// };
 
-//Arrow function
-const calcAge = (birthYear) => 2037 - birthYear;
+// //Arrow function
+// const calcAge = (birthYear) => 2037 - birthYear;
+
+// Coding challenge
+
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+console.log(calcAverage(3, 4, 5));
+
+// Test 1
+const scoreDolphins = calcAverage();
